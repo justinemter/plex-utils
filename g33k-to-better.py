8b1f10f527d05b2c00875494950b3645743b0e1b
Justin <justinemter@gmail.com>
2019-07-01 19:17:58 -0700

    more logic

--- g33k-to-better.py
+++ g33k-to-better.py
@@ -3,6 +3,8 @@ WARNING: THIS WILL ALTER PLEX MEDIA TITLES WHEN RUN. KNOW WHAT YOU ARE DOING.
 
 This script scans specified plex TV libraries searching for "bad_titles" like "g33k" and automatically replaces it with "Series Name Season # Episode - #". 
 
+It also will replace any TV items that are titled by their season # and/or their episode #, prepending the Series title to it.
+
 I recommend commenting out where it says "episode.edit(**new_title)"  on line 31 and do a dry run to see what is printed in your console first. 
 
 Update the two list variables below under "Begin Editable". 
@@ -12,7 +14,7 @@ from plexapi.server import PlexServer
 
 #--- Begin Editable
 baseurl = 'http://192.168.1.103:32400'
-token = '<token>'
+token = '<plex-token>'
 libs = ['TV Shows', 'TV']
 bad_titles = ['g33k']
 #--- End Editable
@@ -26,7 +28,9 @@ for section in sections:
 		for i, media in enumerate(sectionMedia):
 		    episodes = PLEX.library.section(section.title).get(media.title).episodes()
 		    for episode in episodes:
-		    	if episode.title.lower() in (item.lower() for item in bad_titles):
+		    	if episode.title.lower() in (item.lower() for item in bad_titles) \
+		    	or episode.title.lower() is "episode " + str(episode.index) \
+		    	or episode.title.lower() is episode.parentTitle.lower() + " episode " + str(episode.index) :
 		    		new_title = {'title.value': episode.grandparentTitle + " " + episode.parentTitle + " Episode - " + str(episode.index)}
 		    		episode.edit(**new_title)
 		    		episode.reload()
